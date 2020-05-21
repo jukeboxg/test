fa938a99839c3c782182cade64708928c7495706
Jerome Savaria-Carriere <jerome@Diffs-Jeromes-MBP.local>
2020-05-21 11:37:37 -0400

    changed the python code

--- animals.py
+++ animals.py
@@ -1,4 +1,19 @@
-print("Hello world")
-print("Hi again")
+import sys
 
+def default():
+	print('Hello')
 
+def dog():
+	print('Woof')
+
+def cat():
+	print('Meow')
+
+def main():
+	if sys.argv[1] == "dog":
+		dog()
+	else:
+		default()
+
+if __name__ == '__main__':
+	main()
